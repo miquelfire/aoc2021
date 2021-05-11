b346faa43d17559a3e38eb43c440887808eefaf4
Miquel Fire Burns <miquelfire@gmail.com>
2021-05-10 20:54:32 -0400

    Rules apply to config as well
    Well, with VS Code it does.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,14 @@
 module.exports = {
-    "env": {
-        "es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "rules": {
+	'env': {
+		'es2021': true,
+		'node': true
+	},
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 12,
+		'sourceType': 'module'
+	},
+	'rules': {
 		'indent': [
 			'error',
 			'tab',
@@ -28,5 +28,5 @@ module.exports = {
 			'error',
 			'always'
 		]
-    }
+	}
 };
