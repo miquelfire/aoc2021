d5ab9bce83b63d03a6c7a4ded70120971ffe3c54
Miquel Fire Burns <miquelfire@gmail.com>
2021-05-10 20:50:29 -0400

    Copied files from last year
    Also converted to ES6 modules as well.

---
+++ 00.js
@@ -0,0 +1,16 @@
+'use strict';
+/**
+ * @param {string} d 
+ */
+export const part1 = async d => {
+	const data = d.split('\n');
+	return null;
+};
+
+/**
+ * @param {string} d 
+ */
+export const part2 = async d => {
+	const data = d.split('\n');
+	return null;
+};
