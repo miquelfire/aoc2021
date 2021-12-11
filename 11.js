c2870dedbbda0dd9b71c006177ddbe34800cf12d
Miquel Fire Burns <miquelfire@gmail.com>
2021-12-11 09:38:16 -0500

    Day 11 start

---
+++ 11.js
@@ -0,0 +1,16 @@
+'use strict';
+/**
+ * @param {string} d 
+ */
+export const part1 = async d => {
+	const data = d.split('\n').map(e => e.split('').map(e => parseInt(e, 10)));
+	return data;
+};
+
+/**
+ * @param {string} d 
+ */
+export const part2 = async d => {
+	const data = d.split('\n').map(e => e.split('').map(e => parseInt(e, 10)));
+	return data;
+};
