7a041df2aa10a5a3c4afa9cb96cf1c4ff6f4ad81
Miquel Fire Burns <miquelfire@gmail.com>
2021-12-17 12:33:48 -0500

    Day 17 done

--- 17.js
+++ 17.js
@@ -50,7 +50,6 @@ export const part1 = async d => {
 			}
 		}
 	});
-	// At
 
 	return yPosMax;
 };
@@ -59,6 +58,48 @@ export const part1 = async d => {
  * @param {string} d 
  */
 export const part2 = async d => {
-	const [, xMin, xMax, yMin, yMax] = /x=(-?\d+)\.\.(-?\d+), y=(-?\d+)\.\.(-?\d+)/.exec(d);
-	return [yMin, yMax, xMin, xMax];
+	const [, xMin, xMax, yMax, yMin] = /x=(-?\d+)\.\.(-?\d+), y=(-?\d+)\.\.(-?\d+)/.exec(d).map(e => parseInt(e, 10));
+	const step = (pos, vel) => {
+		pos[0] += vel[0];
+		pos[1] += vel[1];
+		if (vel[0] > 0) {
+			vel[0]--;
+		}
+		if (vel[0] < 0) {
+			vel[0]++;
+		}
+		vel[1]--;
+	};
+	const validVel = [];
+
+	// Find x velocities that will land in the target
+	const xVelMax = xMax + 1;
+	const validXVel = new Set;
+	for (let xVel = 1; xVel < xVelMax; xVel++) {
+		const pos = [0, 0];
+		const vel = [xVel, 0]; // Don't care about the y yet
+		while (vel[0] > 0 && pos[0] <= xMax) {
+			step(pos, vel);
+			if (pos[0] >= xMin && pos[0] <= xMax) {
+				validXVel.add(xVel);
+			}
+		}
+	}
+
+	// Now to find xy velcity that hits the target 
+	validXVel.forEach(xVel => {
+		for (let yVel = yMax; yVel < 5000; yVel++) {
+			const pos = [0, 0];
+			const vel = [xVel, yVel];
+			while (pos[0] <= xMax && pos[1] >= yMax) {
+				step(pos, vel);
+				if (pos[0] >= xMin && pos[1] >= yMax && pos[0] <= xMax && pos[1] <= yMin) {
+					validVel.push([xVel, yVel]);
+					break;
+				}
+			}
+		}
+	});
+
+	return validVel.length;
 };
